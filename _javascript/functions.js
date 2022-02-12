ec0d3740f77db791b8e79035dae5f1599929111a
Lucas Loyola <lucas.loyolamatos@gmail.com>
2022-02-12 16:35:37 -0300

    started work

---
+++ _javascript/functions.js
@@ -0,0 +1,62 @@
+/* function formatations */
+
+function verificar() {
+   var data = new Date();
+   var ano = data.getFullYear();
+   var fano = document.getElementById('txtano');
+   var res = document.querySelector('div#res ');
+
+   if (fano.value.length == 0 || fano.value > ano) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!');
+   } else {
+      var fsex = document.getElementsByName('radsex');
+      var idade = ano - Number(fano.value);
+      var genero = ''
+      var img = document.createElement('img');
+      img.setAttribute('id', 'foto');
+
+      if (fsex[0].checked) {
+         genero = 'Homem'
+         if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', '_images/childrenBoy.png');
+         }
+         else if (idade < 21) {
+            // Jovem
+            img.setAttribute('src', '_images/youngMan.png');
+         }
+         else if (idade < 50) {
+            // Adulto
+            img.setAttribute('src', '_images/adultMan.png');
+         }
+         else {
+            // Idoso
+            img.setAttribute('src', '_images/oldMan.png');
+         }
+
+
+
+      } else if (fsex[1].checked) {
+         genero = 'Mulher'
+         if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', '_images/childrenGirl.png');
+         }
+         else if (idade < 21) {
+            // Jovem
+            img.setAttribute('src', '_images/youngWoman.png');
+         }
+         else if (idade < 50) {
+            // Adulto
+            img.setAttribute('src', '_images/adultWoman.png');
+         }
+         else {
+            // Idoso
+            img.setAttribute('src', '_images/oldWoman.png');
+         }
+      }
+      res.style.textAlign = 'center';
+      res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+      res.appendChild(img);
+   }
+}
